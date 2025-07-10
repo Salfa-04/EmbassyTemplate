37b8a2b1d71934799a3e86f20e6f7464a43dd05c
Salfa Chang <me@salfa.cc>
2025-07-10 22:51:23 +0800

    add: iwdg.

--- example/src/tasks/blinky/task.rs
+++ example/src/tasks/blinky/task.rs
@@ -2,18 +2,23 @@
 //! # LED Task
 //!
 
-use crate::{hal::gpio, init_ticker};
+use crate::{hal, init_ticker};
 
 use gpio::{AnyPin, Level, Output as OP, Speed};
+use hal::{gpio, peripherals, wdg};
+use {peripherals::IWDG, wdg::IndependentWatchdog as Dog};
 
 #[embassy_executor::task]
-pub async fn task(p: (AnyPin,)) -> ! {
-    let mut t = init_ticker!(150); // ms
+pub async fn task(p: (IWDG, AnyPin)) -> ! {
+    let mut t = init_ticker!(150); // 150ms
 
-    let mut led = OP::new(p.0, Level::Low, Speed::Low);
+    let mut led = OP::new(p.1, Level::Low, Speed::Low);
+    let mut dog = Dog::new(p.0, 200_000); // 200ms
+
+    dog.unleash(); // Start the WatchDog
 
     loop {
-        led.toggle();
-        t.next().await;
+        (led.toggle(), dog.pet());
+        t.next().await
     }
 }
